57bd711f9cb442d50f09855543d936f5766a4722
minosh00 <it20088682@my.sliit.lk>
2022-05-30 10:53:08 +0530

    push

---
+++ backend/models/UploadFile.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    AssignmentType: { type: String, required: true },
+    Assignment: { type: String, required: true },
+    img: { type: String, required: true }
+});
+
+module.exports = mongoose.model("fileDatabase", fileSchema)
---
+++ backend/server.js
@@ -0,0 +1,58 @@
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+
+
+//pdf
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+
+
+app.use(cors({
+  origin: '*'
+}));
+
+
+app.get("/" , (req,res)=>{
+  res.json("server has started")
+})
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use("/upload", require("./routes/FileUploadRoutess"));
+
+
+
+mongoose.connect(
+  process.env.MDB_CONNECT_STRING, {
+    //type warnings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo DB connected successfully");
+  })
+
+  .catch((err) => console.log("DB connection failed", err));
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
